af1d26c1f1fe4426b69ca8f0f984cb7b412db36e
Sanni Adetola <tonyexplorer202@gmail.com>
2021-12-12 19:07:31 +0100

    react-style

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
+//import necessary dependencies
+
 import $ from 'jquery';
 import './App.css';
 import {useState, useEffect} from "react";
 
 function App() {
-
-  let quotesData = null;
-  // const [quotesData, setQuotesData] = useState(null);
+// initializing state and variables
   const [currentQuote, setCurrentQuote] = useState("");
   const [currentAuthor, setCurrentAuthor] = useState("");
+  const [currentColor, setCurrentColor] = useState("blue");
   const colors = [
     '#16a085',
     '#27ae60',
@@ -23,143 +24,122 @@ function App() {
     '#73A857'
   ];
 
+
+  
+
 // fetch for the quote and set it to quotesData state
-function getQuotes() {
-  console.log('getQuotes')
-  return fetch('https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json',{headers: {Accept: 'application/json'}})
-};
-
-  // get a random quote from quotesData 
- async function getRandomQuote() {
-   const quoteLen = await quotesData.quotes.length
-    const rand = Math.floor(Math.random() * );
-    console.log("getRandQuote")
-    console.log(quotesData.quotes[rand], quotesData.quotes.length, rand)
-    return quotesData.quotes[rand];
-  };
-
- // use the random quote to update the UI
-  function getQuote() {
-    console.log("getQuote")
-    let randomQuote = getRandomQuote();
-  
-    setCurrentQuote(randomQuote.quote);
-    setCurrentAuthor(randomQuote.author);
-    console.log(currentQuote)
-    console.log(currentAuthor)
-
-    $('#tweet-quote').attr(
-      'href',
-      'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' +
-        encodeURIComponent('"' + currentQuote + '" ' + currentAuthor)
-    );
-  
-    $('#tumblr-quote').attr(
-      'href',
-      'https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&caption=' +
-        encodeURIComponent(currentAuthor) +
-        '&content=' +
-        encodeURIComponent(currentQuote) +
-        '&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button'
-    );
-  
-    $('.quote-text').animate({ opacity: 0 }, 400, function () {
-      $(this).animate({ opacity: 1 }, 400);
-      $('#text').text(currentQuote);
-    });
-  
-    $('.quote-author').animate({ opacity: 0 }, 500, function () {
-      $(this).animate({ opacity: 1 }, 400);
-      $('#author').html(currentAuthor);
-    });
-
-  
-    var randomColor = Math.floor(Math.random() * colors.length);
-
-
-    $('html body').animate(
-      {
-        background: colors[randomColor],
-        color: colors[randomColor]
-      },
-      1000
-    );
-    $('.button').animate(
-      { 
-        background: colors[randomColor]
-      },
-      1000
-    );
+  async function getQuotes(animation) {
+    animation()
+    try{
+      // ,{headers: {Accept: 'application/json'}}
+      const data = await fetch('https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json');
+      const quote =  await data.json();
+      // get a random quote from quotesData 
+      let randomQuote = (function() {
+       const quoteLen =  quote.quotes.length;
+       const rand = Math.floor(Math.random() * quoteLen);
+       return quote.quotes[rand];
+     })();
+     let randColor = (function() {
+         return  Math.floor(Math.random() * colors.length);
+     })();
+      setCurrentColor(colors[randColor])
+      setCurrentQuote(randomQuote.quote);
+      setCurrentAuthor(randomQuote.author);
+      console.log(currentColor)
+    }catch(err){
+      console.log(err)
+    }
   };
 
 
-useEffect(()=> {
-
-  getQuotes().then((data) => {
-      return data.json();
-      // return JSON.parse(data)
-
-  }).then(function(parseQuote) {
-        console.log("parseQuote:", parseQuote)
-        quotesData = parseQuote;
-        // setQuotesData(parseQuote);
-       
-  }).then(function(){
-    console.log("stateData:", quotesData)
-    // getQuote();
-  })
-
   
-},[null]);
 
 
 
- 
+ // animate
+  function animation() {
+   
+
+    $('.quote-text').animate({ opacity: 0 }, 10, function(){
+       $(this).animate({opacity:1}, 600)
+    });
+    $('.quote-author').animate({ opacity: 0}, 10, function() {
+       $(this).animate({opacity:1}, 600)
+    });
+   
+
+  };
 
 
+  useEffect(()=> {
+    getQuotes(animation);
+  }, []);
 
   return (
-    <div id="wrapper">
-
-    <div id="quote-box">
-      <div className="quote-text">
-        <i className="fa fa-quote-left"></i>
-        <span id="text"></span>
-      </div>
-      <div className="quote-author">- <span id="author"></span></div>
-      <div className="buttons">
-
-        <div className="share-btn">
-            <a
-              href=""
-              className="button"
-              id="tweet-quote"
-              title="Tweet this quote!"
-              target="_top"
-            >
-              <i className="fa fa-twitter"></i>
-            </a>
-            <a
-               href=""
-              className="button"
-              id="tumblr-quote"
-              title="Post this quote on tumblr!"
-              target="_blank"
-            >
-              <i className="fa fa-tumblr"></i>
-            </a>
-        </div>
   
-        <div className="new-quote-btn">
-          <button className="button" id="new-quote" onClick={getQuote}>New quote</button>
-        </div> 
 
-      </div>
+    
+    <div id="wrapper" style={{background:currentColor}}>
+    <div className="container">
+
+       
+          <div id="quote-box">
+            <div className="quote-text" style={{opacity:0}}>
+              <i className="fa fa-quote-left" style={{color:currentColor}}></i>
+              <span id="text" style={{color:currentColor}}>{currentQuote ? currentQuote: "Loading..." }</span>
+            </div>
+            <div className="quote-author" style={{color:currentColor, opacity: 0}}>- <span id="author">{currentAuthor ? currentAuthor : "Loading..."}</span></div>
+            <div className="buttons">
+
+              <div className="share-btn">
+                  <a
+                   style={{background:currentColor}}
+                    href={ 'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' +
+                    encodeURIComponent('"' + currentQuote + '" ' + currentAuthor)}
+                    className="button"
+                    id="tweet-quote"
+                    title="Tweet this quote!"
+                    target="_top"
+                    rel="noreferrer noopener" 
+                  >
+                    <i className="fa fa-twitter"></i>
+                  </a>
+                  <a
+                    style={{background:currentColor}}
+                    href={'https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&caption=' +
+                    encodeURIComponent(currentAuthor) +
+                    '&content=' +
+                    encodeURIComponent(currentQuote) +
+                    '&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button'}
+                    className="button"
+                    id="tumblr-quote"
+                    title="Post this quote on tumblr!"
+                    target="_blank"
+                    rel="noreferrer noopener" 
+                  >
+                    <i className="fa fa-tumblr"></i>
+                  </a>
+              </div>
+        
+              <div className="new-quote-btn">
+                <button className="button" id="new-quote"  style={{background:currentColor}} onClick={()=> getQuotes(animation)}>New quote</button>
+              </div> 
+
+            </div>
     </div>
   
-    <div className="footer">by <a href="https://codepen.io/Kruze-Development/">tonyKuz</a></div>
+      <div className="footer">by <a href="https://codepen.io/Kruze-Development/">tonyKuz</a></div>
+    </div>
   </div>
   );
 }
 
-export default App;
+
+//render the root ui to the DOM
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
